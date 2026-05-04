13c675ca3df8eb556b5612d10556e1d74b4de669
w30096 <52794968+w30096@users.noreply.github.com>
2026-05-04 22:41:25 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// 讓雲端伺服器能讀取當前資料夾的檔案
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -14,12 +13,11 @@ app.get('/', (req, res) => {
 let players = {}; 
 
 io.on('connection', (socket) => {
-    console.log('玩家連線:', socket.id);
+    console.log('連線成功:', socket.id);
 
     socket.on('ready', (data) => {
         players[socket.id] = { fleet: data.fleet, ready: true };
         const ids = Object.keys(players);
-        // 當兩位玩家都準備好，通知遊戲開始，由第一位進攻
         if (ids.length === 2 && players[ids[0]].ready && players[ids[1]].ready) {
             io.emit('gameStart', ids[0]); 
         }
@@ -38,8 +36,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// 關鍵修改：支援雲端動態分配 Port
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
-    console.log(`海戰伺服器已啟動！Port: ${PORT}`);
+    console.log(`伺服器運作中 Port: ${PORT}`);
 });
